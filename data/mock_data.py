8e7cfcd4691b5590d5ef479951c884be1c1a62e6
blueduck0907 <blueduck09072004@gmail.com>
2025-10-23 15:14:23 +0700

    Update mock_data.py

--- data/mock_data.py
+++ data/mock_data.py
@@ -9,7 +9,7 @@ def get_system_info():
     """Trả về thông tin hệ thống giả"""
     return {
         "system_info": {
-            "name": "Demo FISS API v16777",
+            "name": "Demo FISS API v167777",
             "version": "1.0.0",
             "environment": "development"
         },
